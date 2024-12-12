b17b41d6b73cff3097eef7dd998c6382785044f9
fayzi-dev <fayzi.dev@gmail.com>
2024-12-11 23:04:16 -0800

    Inheritance use virtual and  override method in Solidity

--- Contracts/Inheritance2.sol
+++ Contracts/Inheritance2.sol
@@ -7,4 +7,13 @@ contract Child is Parent{
     function GetSum(uint c, uint d) public pure returns (uint){
         return  SumNumber(c,d);
     }
+
+    function SumNumber(uint a, uint b) public  pure override returns (uint){
+        if(a > 5){
+            return a + b;
+
+        }else {
+            return a + b + b;
+        }
+    }
 }

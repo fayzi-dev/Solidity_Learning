8422f1170568bb3c5fae6ed8244f75320c4e0dae
fayzi-dev <fayzi.dev@gmail.com>
2024-12-11 22:50:30 -0800

    Library in Solidity

---
+++ Contracts/Library.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+library Sum{
+
+    function SumNumber(uint number1, uint number2) internal pure returns(uint){
+        return number1 + number2;
+    }
+}
